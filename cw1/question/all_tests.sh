5a160214b189bc7401cecf7353a80d7e8d6ec9dd
Dominic Moylett <dm1905@bristol.ac.uk>
2014-02-07 16:59:21 +0000

    Added debug target to makefile.

--- cw1/question/all_tests.sh
+++ cw1/question/all_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-make all
+make debug
 sh test.sh stage1
 sh test.sh stage2
+sh test.sh stage3
 sh test.sh stage4
 make clean
--- cw1/question/test.sh
+++ cw1/question/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./modmul $1 < $1.input > output.txt
+./debug $1 < $1.input > output.txt
 diff output.txt $1.output
 rm output.txt

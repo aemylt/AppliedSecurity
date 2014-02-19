ab5f8cc0fc57344ae491ada762a99cb6f5d4a64f
Dominic Moylett <dm1905@bristol.ac.uk>
2014-02-19 11:13:03 +0000

    Converted use of mpz_t for omega to uint64_t.

--- cw1/question/modmul.c
+++ cw1/question/modmul.c
@@ -7,18 +7,17 @@ inline int test_bit(mpz_t y, int64_t i) {
 }
 
 // Create variables rho and omega for Montgomery methods
-void mont_init_cube(mpz_t rho2, mpz_t rho3, mpz_t omega, mpz_t N) {
-  uint64_t i;
+void mont_init_cube(mpz_t rho2, mpz_t rho3, uint64_t *omega, mpz_t N) {
+  uint64_t i, tmp;
   mpz_t sub_op;
   mpz_init_set_ui(sub_op, 1);
   mpz_mul_2exp(sub_op, sub_op, 64);
-  mpz_set_ui(omega, 1);
+  tmp = 1;
   for (i = 1; i < 64; i++) {
-    mpz_mul(omega, omega, omega);
-    mpz_mul(omega, omega, N);
-    mpz_set_ui(omega, omega->_mp_d[0]);
+    tmp *= tmp;
+    tmp *= N->_mp_d[0];
   }
-  mpz_sub(omega, sub_op, omega);
+  *omega = 0 - tmp;
   mpz_set_ui(rho2, 1);
   for (i = 1; i <= N->_mp_size << 7; i++) {
     mpz_add(rho2, rho2, rho2);
@@ -32,18 +31,17 @@ void mont_init_cube(mpz_t rho2, mpz_t rho3, mpz_t omega, mpz_t N) {
 }
 
 // Create variables rho and omega for Montgomery methods
-void mont_init(mpz_t rho2, mpz_t omega, mpz_t N) {
-  uint64_t i;
+void mont_init(mpz_t rho2, uint64_t *omega, mpz_t N) {
+  uint64_t i, tmp;
   mpz_t sub_op;
   mpz_init_set_ui(sub_op, 1);
   mpz_mul_2exp(sub_op, sub_op, 64);
-  mpz_set_ui(omega, 1);
+  tmp = 1;
   for (i = 1; i < 64; i++) {
-    mpz_mul(omega, omega, omega);
-    mpz_mul(omega, omega, N);
-    mpz_set_ui(omega, omega->_mp_d[0]);
+    tmp *= tmp;
+    tmp *= N->_mp_d[0];
   }
-  mpz_sub(omega, sub_op, omega);
+  *omega = 0 - tmp;
   mpz_set_ui(rho2, 1);
   for (i = 1; i <= N->_mp_size << 7; i++) {
     mpz_add(rho2, rho2, rho2);
@@ -52,22 +50,18 @@ void mont_init(mpz_t rho2, mpz_t omega, mpz_t N) {
 }
 
 // Perform modular multiplication via Montgomery reduction
-void mont_mul(mpz_t r, mpz_t x, mpz_t y, mpz_t N, mpz_t omega) {
-  mpz_t tmp, yi_x, u;
-  uint64_t i, j;
+void mont_mul(mpz_t r, mpz_t x, mpz_t y, mpz_t N, uint64_t omega) {
+  mpz_t tmp, t, yi_x;
+  uint64_t i, j, u;
   mpz_init_set_ui(tmp, 0);
-  mpz_init(u);
   mpz_init(yi_x);
+  mpz_init(t);
   for (i = 0; i < N->_mp_size; i++) {
-    mpz_set_ui(u, x->_mp_d[0]);
-    mpz_mul_ui(u, u, (y->_mp_size > i) ? y->_mp_d[i] : 0);
-    mpz_add_ui(u, u, tmp->_mp_d[0]);
-    mpz_mul(u, u, omega);
-    mpz_set_ui(u, u->_mp_d[0]);
-    mpz_mul(u, u, N);
+    u = (x->_mp_d[0] * ((y->_mp_size > i) ? y->_mp_d[i] : 0) + tmp->_mp_d[0]) * omega;
+    mpz_mul_ui(t, N, u);
     mpz_mul_ui(yi_x, x, (y->_mp_size > i) ? y->_mp_d[i] : 0);
     mpz_add(tmp, tmp, yi_x);
-    mpz_add(tmp, tmp, u);
+    mpz_add(tmp, tmp, t);
     for (j = 0; j < tmp->_mp_size - 1; j++) {
       tmp->_mp_d[j] = tmp->_mp_d[j + 1];
     }
@@ -78,15 +72,14 @@ void mont_mul(mpz_t r, mpz_t x, mpz_t y, mpz_t N, mpz_t omega) {
 }
 
 // Perform a modular operation via Montgomery Reduction
-void mont_red(mpz_t x, mpz_t N, mpz_t omega) {
-  mpz_t u;
-  uint64_t i, j;
-  mpz_init(u);
+void mont_red(mpz_t x, mpz_t N, uint64_t omega) {
+  uint64_t i, j, u;
+  mpz_t tmp;
+  mpz_init(tmp);
   for (i = 0; i < N->_mp_size; i++) {
-    mpz_mul_ui(u, omega, x->_mp_d[0]);
-    mpz_set_ui(u, u->_mp_d[0]);
-    mpz_mul(u, u, N);
-    mpz_add(x, x, u);
+    u = x->_mp_d[0] * omega;
+    mpz_mul_ui(tmp, N, u);
+    mpz_add(x, x, tmp);
     for (j = 0; j < x->_mp_size - 1; j++) {
       x->_mp_d[j] = x->_mp_d[j + 1];
     }
@@ -95,7 +88,7 @@ void mont_red(mpz_t x, mpz_t N, mpz_t omega) {
   if (mpz_cmp(x, N) > -1) mpz_sub(x, x, N);
 }
 
-void exp_mod_mont(mpz_t r, mpz_t x, mpz_t y, mpz_t N, mpz_t rho2, mpz_t omega) {
+void exp_mod_mont(mpz_t r, mpz_t x, mpz_t y, mpz_t N, mpz_t rho2, uint64_t omega) {
   mpz_t tmp, mp_u, and_op;
   int64_t i, j, l;
   uint64_t u;
@@ -148,16 +141,16 @@ void exp_mod_mont(mpz_t r, mpz_t x, mpz_t y, mpz_t N, mpz_t rho2, mpz_t omega) {
 
 // Compute r = x^y (mod N) via sliding window.
 void exp_mod_cube(mpz_t r, mpz_t x, mpz_t y, mpz_t N) {
-  mpz_t rho2, rho3, omega, x_tmp;
+  mpz_t rho2, rho3, x_tmp;
+  uint64_t omega;
 
   mpz_init(x_tmp);
   mpz_set(x_tmp, x);
 
   mpz_init(rho2);
   mpz_init(rho3);
-  mpz_init(omega);
 
-  mont_init_cube(rho2, rho3, omega, N);
+  mont_init_cube(rho2, rho3, &omega, N);
   mont_mul(x_tmp, x_tmp, rho3, N, omega);
   mont_red(x_tmp, N, omega);
 
@@ -175,7 +168,8 @@ Perform stage 1:
 
 void stage1() {
 
-  mpz_t N, e, m, c, rho2, omega;
+  mpz_t N, e, m, c, rho2;
+  uint64_t omega;
 
   // Initialise integers
   mpz_init(N);
@@ -183,7 +177,6 @@ void stage1() {
   mpz_init(m);
   mpz_init(c);
   mpz_init(rho2);
-  mpz_init(omega);
 
   // Repeat until we reach end of stream
   while(gmp_scanf("%ZX", N) > 0) {
@@ -192,7 +185,7 @@ void stage1() {
     gmp_scanf("%ZX", m);
 
     // c = m^e (mod N)
-    mont_init(rho2, omega, N);
+    mont_init(rho2, &omega, N);
     mont_mul(m, m, rho2, N, omega);
     exp_mod_mont(c, m, e, N, rho2, omega);
     mont_red(c, N, omega);
@@ -274,7 +267,8 @@ Perform stage 3:
 
 void stage3() {
 
-  mpz_t p, q, g, h, m, c_1, c_2, w, rho2, omega;
+  mpz_t p, q, g, h, m, c_1, c_2, w, rho2;
+  uint64_t omega;
 #ifndef DEBUG
   gmp_randstate_t state;
 #endif
@@ -288,7 +282,6 @@ void stage3() {
   mpz_init(c_1);
   mpz_init(c_2);
   mpz_init(rho2);
-  mpz_init(omega);
 
 #ifndef DEBUG
   mpz_init(w);
@@ -314,7 +307,7 @@ void stage3() {
 
     // c_1 = g^w (mod p)
     // c_2 = m * h^w (mod p)
-    mont_init(rho2, omega, p);
+    mont_init(rho2, &omega, p);
     mont_mul(g, g, rho2, p, omega);
     exp_mod_mont(c_1, g, w, p, rho2, omega);
     mont_red(c_1, p, omega);
@@ -343,7 +336,8 @@ Perform stage 4:
 
 void stage4() {
 
-  mpz_t p, q, g, x, c_1, c_2, m, tmp, rho2, omega;
+  mpz_t p, q, g, x, c_1, c_2, m, tmp, rho2;
+  uint64_t omega;
 
   // Initialise integers
   mpz_init(p);
@@ -355,7 +349,6 @@ void stage4() {
   mpz_init(m);
   mpz_init(tmp);
   mpz_init(rho2);
-  mpz_init(omega);
 
   // Repeat until we reach end of stream
   while (gmp_scanf("%ZX", p) > 0) {
@@ -367,7 +360,7 @@ void stage4() {
     gmp_scanf("%ZX", c_2);
 
     // m = c_1^(q-x) * c_2 (mod p)
-    mont_init(rho2, omega, p);
+    mont_init(rho2, &omega, p);
     mpz_sub(tmp, q, x);
 
     mont_mul(c_1, c_1, rho2, p, omega);

3d2f03929e6590f78c9bbfb3a1ed81e31d31a746
Dominic Moylett <dm1905@bristol.ac.uk>
2014-03-11 18:28:00 +0000

    Replaced division with floor division operator.

--- cw2/dm1905/oaep/oaep.py
+++ cw2/dm1905/oaep/oaep.py
@@ -4,10 +4,7 @@ import subprocess
 import math
 
 def ceildiv(x, y):
-    q, r = divmod(x, y)
-    if r != 0:
-        q += 1
-    return q
+    return -(-x//y)
 
 def interact(f):
     c_prime = (c * pow(f, e, N)) % N
@@ -28,12 +25,13 @@ public = open(sys.argv[2], 'r')
 N_hex = public.readline()
 e_hex = public.readline()
 c_hex = public.readline()
+public.close()
 
 N = int(N_hex, 16)
 e = int(e_hex, 16)
 c = int(c_hex, 16)
 
-k = len(N_hex) / 2
+k = len(N_hex) // 2
 B = pow(2, 8 * (k - 1))
 
 target = subprocess.Popen(args   = sys.argv[ 1 ],
@@ -53,26 +51,26 @@ while l != 1:
     l = interact(f_1)
     counter += 1
 
-f_2 = int((N + B) / B) * (f_1 / 2)
+f_2 = int((N + B) // B) * (f_1 // 2)
 
 while l == 1:
     l = interact(f_2)
     if l == 1:
-        f_2 = f_2 + f_1 / 2
+        f_2 = f_2 + f_1 // 2
     counter += 1
 
 m_min = ceildiv(N, f_2)
-m_max = (N + B) / f_2
+m_max = (N + B) // f_2
 
 while m_min != m_max:
-    f_tmp = 2 * B / (m_max - m_min)
-    i = (f_tmp * m_min) / N
+    f_tmp = 2 * B // (m_max - m_min)
+    i = (f_tmp * m_min) // N
     f_3 = ceildiv(i * N, m_min)
     l = interact(f_3)
     if l == 1:
         m_min = ceildiv(i * N + B, f_3)
     else:
-        m_max = (i * N + B) / f_3
+        m_max = (i * N + B) // f_3
     counter += 1
 
 m = m_min

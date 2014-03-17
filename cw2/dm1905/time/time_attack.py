946969f5d94f57a1c27edfd9a4f9966390063527
Dominic Moylett <dm1905@bristol.ac.uk>
2014-03-17 23:57:12 +0000

    Montgomery multiplication implemented in python.

--- cw2/dm1905/time/time_attack.py
+++ cw2/dm1905/time/time_attack.py
@@ -3,6 +3,14 @@ import sys
 import subprocess
 import random
 
+b = 18446744073709551616
+l_b = 64
+
+# Helper function for ceiling division
+# Works by upside-down floor division
+def ceildiv(x, y):
+    return -(-x//y)
+
 # Helper function for interacting with the system
 # Converts an integer into a hexadecimal string, does any necessary padding
 # and returns the result as a string.
@@ -13,6 +21,40 @@ def interact(c):
     l = int(target_out.readline().strip())
     m = int(target_out.readline().strip(), 16)
     return l, m
+
+def mont_init(N):
+    l_N = ceildiv(len("%X" % N), 16)
+    rho_2 = 1
+    for i in range(2 * l_N * l_b):
+        rho_2 += rho_2
+        if rho_2 >= N:
+            rho_2 -= N
+    omega = 1
+    for i in range(1, l_b):
+        omega = (omega * omega * N) & (b - 1)
+    omega = b - omega
+    return l_N, rho_2, omega
+
+def mont_mul(x, y, N, l_N, omega):
+    r = 0
+    for i in range(l_N):
+        u = (((r & (b - 1)) + ((y >> (i * l_b)) & (b - 1)) * (x & (b - 1))) * omega) & (b - 1)
+        r = (r + ((y >> (i * l_b)) & (b - 1)) * x + u * N) >> l_b
+    if r > N:
+        return r - N, True
+    else:
+        return r, False
+
+def mont_exp(x, y, N, rho_2, l_N, omega):
+    x_p, _ = mont_mul(x, rho_2, N, l_N, omega)
+    t, _ = mont_mul(1, rho_2, N, l_N, omega)
+
+    for i in range(len("%X" % y) * 4 - 1, -1, -1):
+        t, _ = mont_mul(t, t, N, l_N, omega)
+        if (y >> i) & 1:
+            t, _ = mont_mul(t, x_p, N, l_N, omega)
+    r, _ = mont_mul(t, 1, N, l_N, omega)
+    return r
 
 # Get N and e
 public = open(sys.argv[2], 'r')
@@ -30,10 +72,13 @@ target = subprocess.Popen(args   = sys.argv[ 1 ],
 target_out = target.stdout
 target_in  = target.stdin
 
+l_N, rho_2, omega = mont_init(N)
+
+c = random.randint(0, N)
+test = mont_exp(c, e, N, rho_2, l_N, omega)
+
+print test
+print pow(c, e, N)
+
 c = random.randint(0, N)
 l, m = interact(c)
-
-print c
-
-print l
-print m
